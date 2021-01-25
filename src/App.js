915d890fe73428f6a54601e978d5bd4576eece5b
Mayank Chugh <mayankchugh1977@gmail.com>
2021-01-26 07:22:56 +0800

    before create element

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 
-class  App extends Component {
-    render (){
-      return (
+class App extends Component {
+  render() {
+    let x = 2;
+    let y = 3;
+    let moduleName="React";
+
+    return (
       <div>
-        Phone Directory
+        {/* <div className="header">
+          Phone Directory
+          </div>
+        <button> Add </button>
+        <div>
+          <span> Name </span><br />
+          <span> Phone </span>
+        </div> */}
+        <span>Learning {moduleName} is so much fun! {Math.pow(x,y)}  </span><br/><br/>
+        <label htmlFor="name"> Name: </label>
+        <input id="name" type="text" placeholder="Type Here" defaultValue={x+y}/>
       </div>
+
     );
-}
+  }
 }
 
 export default App;
