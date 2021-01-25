0622db08aca366e415582a62fb8024b91e1f550d
Mayank Chugh <mayankchugh1977@gmail.com>
2021-01-26 07:26:51 +0800

    first create element done and deleting it now

--- src/App.js
+++ src/App.js
@@ -7,20 +7,13 @@ class App extends Component {
     let moduleName="React";
 
     return (
-      <div>
-        {/* <div className="header">
-          Phone Directory
-          </div>
-        <button> Add </button>
-        <div>
-          <span> Name </span><br />
-          <span> Phone </span>
-        </div> */}
-        <span>Learning {moduleName} is so much fun! {Math.pow(x,y)}  </span><br/><br/>
-        <label htmlFor="name"> Name: </label>
-        <input id="name" type="text" placeholder="Type Here" defaultValue={x+y}/>
-      </div>
+      // <div id="module">
+      //   <p>ReactJS</p>
+      // </div>
 
+        React.createElement("div", {id: "module"},
+        React.createElement("p", null, "ReactJS")
+        )
     );
   }
 }
