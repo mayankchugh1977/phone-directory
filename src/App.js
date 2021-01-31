8cdc9a8768baa69a58497b0cba7c59fba6ff70f4
Mayank Chugh <mayankchugh1977@gmail.com>
2021-02-01 06:50:47 +0800

    before Add subscriber work

--- src/App.js
+++ src/App.js
@@ -1,17 +1,53 @@
 import React, { Component } from 'react';
 import Header from "./Header";
+import "./App.css";
 
 class App extends Component {
+
+  clickHandler (message){
+    alert(message );
+  }
+
   render() {
 
+    let subscribers = [
+      {
+        id: 1,
+        name: "Shipla Bhatt",
+        phone: "8888888888"
+      },
+      {
+        id: 2,
+        name: "Srishti",
+        phone: "9999999999"
+      }
+    ]
+
     return (
-      <div>
-        <Header/>
-        <button> Add </button>
-        <div>
-          <span> Name </span><br />
-          <span> Phone </span>
-        </div>        
+      <div className="component-container">
+        <Header heading="Phone Directory"/>
+        <div className="component-body-container">
+          <button className="custom-btn add-btn"> Add </button>
+          
+          <div className="grid-container heading-container">
+            <span className="grid-item name-heading" > Name </span>
+            <span className="grid-item phone-heading"> Phone </span>
+          </div>  
+
+          {
+            subscribers.map(sub => {
+              return  <div key = {sub.id} className="grid-container">
+                        <span className="grid-item" > {sub.name} </span>
+                        <span className="grid-item"> {sub.phone}  </span>
+                        <span className="grid-item action-btn-container">
+                          <button className="custom-btn delete-btn" onClick={this.clickHandler.bind(this, sub.id)} > Delete</button>
+                        </span>
+                       
+                      </div>
+            })
+          }
+
+        </div>  
       </div>
     );
   }
--- src/Header.js
+++ src/Header.js
@@ -1,24 +1,27 @@
-import React from 'react';
+// import React from 'react';
+// import './Header.css';
+// 
+// const Header = function(props){
+// 
+//     const headerStyle = {textAlign: 'center', padding:20, background: '#000', color: '#FFF', textTransform: 'uppercase'};
+//     return(
+//         <div className="header">
+//             {props.heading}
+//         </div>
+//     )
+// }
+
+import React, {Component} from 'react';
 import './Header.css';
 
-const Header = function(){
-
-    const headerStyle = {textAlign: 'center', padding:20, background: '#000', color: '#FFF', textTransform: 'uppercase'};
-    return(
-        <div className="header">
-            Phone Directory
-        </div>
-    )
+class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                {this.props.heading}
+            </div>
+        )
+    }
 }
-
-// class Header extends Component {
-//     render() {
-//         return (
-//             <div className="header">
-//                 Phone Directory
-//             </div>
-//         )
-//     }
-// }
 
 export default Header;
